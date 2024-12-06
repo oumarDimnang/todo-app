9074424e9741e0bf2fd73de66d43623055a6a263
Oumar Dimnang <oumar.dimnang@gmail.com>
2024-12-06 17:33:15 +0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+require("dotenv").config();
+const taskRoutes = require("./routes/taskRoute")
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors())
+app.use(bodyParser.json())
+app.use(taskRoutes)
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello from the backend!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`)
+})
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch((err => console.log(err)))
